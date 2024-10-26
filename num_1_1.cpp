67b1ca4524f5a0bf77501d98a5610198ee84ab3a
Shamil Krut <gmific@yandex.ru>
2024-10-26 12:00:33 -0700

    Create num_1_1.cpp

---
+++ num_1_1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+float mean(const vector<float>&vec);
+
+float mean(const vector<float>&vec)
+{
+    float sum = 0;
+    for (int i = 0; i < vec.size(); ++i)
+    {
+        sum += vec[i];
+    }
+    return sum/float(vec.size());
+}
+
+
+int main()
+{
+    int size;
+    cin >> size;
+    vector<float> vec(size);
+    
+    for(int i = 0; i < size; ++i) {cin >> vec[i];}
+    
+    cout << mean(vec) << endl;
+    return 0;
+}
