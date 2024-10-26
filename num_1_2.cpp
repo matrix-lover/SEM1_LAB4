91758a5e42ea8ae8e794d0dfb60b08dbc16f1143
Shamil Krut <gmific@yandex.ru>
2024-10-26 13:04:46 -0700

    Update num_1_2.cpp

--- num_1_2.cpp
+++ num_1_2.cpp
@@ -1,24 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include "lab04.hpp"
 
 using namespace std;
 
 float mean(const vector<float>&vec);
-
-template<class T>
-vector<T> generate_random_vector(size_t num, T min = -100, T max = 100)
-{
-        random_device rd;
-        mt19937 e2(rd());
-        uniform_real_distribution<> dist(-100, 100);
-
-        auto lambda = [&e2, &dist] () -> T { return dist(e2); };
-
-        vector<T> result(20);
-        generate_n(result.begin(), num, lambda);
-        return result;
-}
 
 float mean(const vector<float>&vec)
 {
@@ -30,10 +17,10 @@ float mean(const vector<float>&vec)
     return sum/float(vec.size());
 }
 
-
 int main() {
     vector<float> vec = generate_random_vector<float>(20, -100, 100);
     
+    foo(vec); // начальные значения
     cout << mean(vec) << endl;
     return 0;
 }
