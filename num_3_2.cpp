6c955e5013fb605cb62fd53dadb77495860152e3
Shamil Krut <gmific@yandex.ru>
2024-10-26 13:00:54 -0700

    Update num_3_2.cpp

--- num_3_2.cpp
+++ num_3_2.cpp
@@ -1,24 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include "lab04.hpp"
 
 using namespace std;
 
 float argMax(const vector<float>&vec);
-
-template<class T>
-vector<T> generate_random_vector(size_t num, T min = -100, T max = 100)
-{
-        random_device rd;
-        mt19937 e2(rd());
-        uniform_real_distribution<> dist(-100, 100);
-
-        auto lambda = [&e2, &dist] () -> T { return dist(e2); };
-
-        vector<T> result(10);
-        generate_n(result.begin(), num, lambda);
-        return result;
-}
 
 int argMax(const vector<int>&vec)
 {
@@ -39,10 +26,7 @@ int argMax(const vector<int>&vec)
 int main() {
     vector<int> vec = generate_random_vector<int>(10, -100, 100);
 
-    for(int i = 0; i < vec.size(); ++i) // начальные значения
-    {
-        cout << i+1 << ": " << vec[i]<< endl;
-    }
+    foo(vec);
     cout << '\n';
     
     if (vec.size() > 0)
@@ -50,7 +34,7 @@ int main() {
         cout << "Индекс максимального: " << argMax(vec)+1 << endl;
     }
     else {cout << -1 << endl;}
+    
     return 0;
 }
-
 
