f3ec59d7efbaa6cfc3622a3eb22accf33e6b88e9
Shamil Krut <gmific@yandex.ru>
2024-10-26 13:49:27 -0700

    Update num_2_2.cpp

--- num_2_2.cpp
+++ num_2_2.cpp
@@ -5,8 +5,6 @@
 
 using namespace std;
 
-pair<float, float> minMax(const vector<float>&vec);
-
 pair<float, float> minMax(const vector<float>&vec)
 {
     pair<float, float> value (vec[0], vec[0]);
