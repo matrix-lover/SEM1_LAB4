83bbc22caaf76b495fe36539b6427c8acb9c9eb1
Shamil Krut <gmific@yandex.ru>
2024-10-26 12:18:01 -0700

    Update num_3_1.cpp

--- num_3_1.cpp
+++ num_3_1.cpp
@@ -8,15 +8,20 @@ float argMax(const vector<float>&vec);
 template< class T > class numeric_limits;
 
 
-float argMax(const vector<float>&vec)
+int argMax(const vector<int>&vec)
 {
-    float max = 0;
+    int max = 0;
+    int max_index = 0;
     max = vec[0];
     for (int i = 0; i < vec.size(); ++i)
     {
-        if (vec[i] > max) {max = i;}
+        if (vec[i] > max)
+        {
+            max = vec[i];
+            max_index = i;
+        }
     }
-    return max;
+    return max_index;
 }
 
 int main()
@@ -24,12 +29,12 @@ int main()
     
     int size;
     cin >> size;
-    vector<float> vec(size);
+    vector<int> vec(size);
         
     for(int i = 0; i < size; ++i){cin >> vec[i];}
-    if (size > 0)
+    if (vec.size() > 0)
     {
-        cout << argMax(vec) << endl;
+        cout << "Индекс максимального: " << argMax(vec)+1 << endl;
     }
     else {cout << -1 << endl;}
     return 0;
