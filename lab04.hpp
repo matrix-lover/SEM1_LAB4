9ece18bd5ee973ec1bff0fe08b160c4dc87e103d
Shamil Krut <gmific@yandex.ru>
2024-10-26 12:54:58 -0700

    Update lab04.hpp

--- lab04.hpp
+++ lab04.hpp
@@ -1,10 +1,26 @@
 using namespace std;
 
-void foo(const vector<int>&vec)
+
+template<class T1>
+void foo(const vector<T1>&vec)
 {
     for(int i = 0; i < vec.size(); ++i) // начальные или конечные значения
     {
         cout << i+1 << ": " << vec[i]<< endl;
     }
     cout << '\n';
+}
+
+template<class T2>
+vector<T2> generate_random_vector(size_t num, T2 min = -100, T2 max = 100)
+{
+        random_device rd;
+        mt19937 e2(rd());
+        uniform_real_distribution<> dist(-100, 100);
+
+        auto lambda = [&e2, &dist] () -> T2 { return dist(e2); };
+
+        vector<T2> result(10);
+        generate_n(result.begin(), num, lambda);
+        return result;
 }
