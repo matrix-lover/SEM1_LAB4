800d1ed30269777ce2e41107bd27337eb76c8bae
Shamil Krut <gmific@yandex.ru>
2024-10-26 12:34:14 -0700

    Create lab04.hpp

---
+++ lab04.hpp
@@ -0,0 +1,10 @@
+using namespace std;
+
+void foo(const vector<int>&vec)
+{
+    for(int i = 0; i < vec.size(); ++i) // начальные значения
+    {
+        cout << i+1 << ": " << vec[i]<< endl;
+    }
+    cout << '\n' << '\n';
+}
