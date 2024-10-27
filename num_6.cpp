da5a3530a3807ff333da9ac92751d7142147a326
Shamil Krut <gmific@yandex.ru>
2024-10-27 00:17:22 -0700

    Update num_6.cpp

--- num_6.cpp
+++ num_6.cpp
@@ -1,1 +1,34 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+string replace(string&str, string&old, string&new_string)
+{
+    int simb = 0;
+    while(old[simb] != 0)
+    {
+        simb += 1;
+    }
+    while(str.find(old) != string::npos){
+        unsigned long start = str.find(old);
+        str.erase(start, simb);
+        str.insert(start, new_string);
+    }
+    return str;
+}
+
+int main() {
+    string str;
+    string old;
+    string new_string;
+    getline(cin, str);
+    cin >> old;
+    cin >> new_string;
+    cout << '\n';
+    
+    replace(str, old, new_string);
+    
+    cout << str << endl;
+    return 0;
+}
 
